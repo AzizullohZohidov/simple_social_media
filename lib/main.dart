98450621e2e50ea300618103511538b89e69414c
AzizullohZohidov <zohidov.azizulloh@gmail.com>
2021-11-28 09:03:21 +0500

    Fixes, Connected to Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,40 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:simple_social_media/presentation/screens/Login_screen/log_in_screen.dart';
 import 'package:simple_social_media/presentation/screens/Signup_screen/sign_up_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+  MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Simple Social Media App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: SignUpScreen(),
-      //home: LogInScreen(),
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const MaterialApp(
+            home: Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(),
+              ),
+            ),
+          );
+        }
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Simple Social Media App',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: SignUpScreen(),
+          //home: LogInScreen(),
+        );
+      },
     );
   }
 }
