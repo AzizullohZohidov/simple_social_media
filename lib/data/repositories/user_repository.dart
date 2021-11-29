adff7077f46979cc5f9983d6f0cf11589fab804a
AzizullohZohidov <zohidov.azizulloh@gmail.com>
2021-11-30 00:00:38 +0500

    User Repository -getCurrentUser -getUserUnderId

---
+++ lib/data/repositories/user_repository.dart
@@ -0,0 +1,38 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:simple_social_media/data/models/user_model.dart';
+
+class UserRepository {
+  late FirebaseAuth _auth;
+  AuthenticationRepository() {
+    _auth = FirebaseAuth.instance;
+  }
+
+  //Read current user info
+  Future<UserModel?> getCurrentUser() async {
+    try {
+      return await getUserUnderId(FirebaseAuth.instance.currentUser!.uid);
+    } catch (error) {}
+  }
+
+  //Read user info given user id
+  Future<UserModel?> getUserUnderId(String uid) async {
+    UserModel user;
+    try {
+      FirebaseFirestore.instance
+          .collection('users')
+          .doc(uid)
+          .get()
+          .then<dynamic>((DocumentSnapshot snapshot) async {
+        user = UserModel(
+          id: snapshot.get('id'),
+          email: snapshot.get('email'),
+          firstName: snapshot.get('firstName'),
+          lastName: snapshot.get('lastName'),
+          createdAt: snapshot.get('createdAt').toDate(),
+        );
+        return user;
+      });
+    } catch (error) {}
+  }
+}
