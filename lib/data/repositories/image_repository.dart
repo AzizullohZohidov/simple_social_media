5c5e50802e1988fc138740115dfa0d2709fae945
AzizullohZohidov <zohidov.azizulloh@gmail.com>
2021-11-29 19:58:48 +0500

    Image Repository is Implemented

---
+++ lib/data/repositories/image_repository.dart
@@ -0,0 +1,37 @@
+import 'dart:io';
+
+import 'package:image_cropper/image_cropper.dart';
+import 'package:image_picker/image_picker.dart';
+
+class ImageRepository {
+  Future<File?> _pickImageCamOrGallery(ImageSource source) async {
+    PickedFile? pickedFile;
+    var xPhoto = await ImagePicker().pickImage(
+      source: source,
+      maxHeight: 1080,
+      maxWidth: 1080,
+    );
+    if (xPhoto != null) {
+      pickedFile = PickedFile(xPhoto.path);
+      var _croppedImage = await _cropImage(pickedFile.path);
+      return _croppedImage;
+    }
+  }
+
+  Future<File?> _pickImageFromGallery() async {
+    return await _pickImageCamOrGallery(ImageSource.gallery);
+  }
+
+  Future<File?> _takePhoto() async {
+    return await _pickImageCamOrGallery(ImageSource.camera);
+  }
+
+  Future<File?> _cropImage(filePath) async {
+    File? croppedImage = await ImageCropper.cropImage(
+      sourcePath: filePath,
+      maxHeight: 1080,
+      maxWidth: 1080,
+    );
+    return croppedImage;
+  }
+}
